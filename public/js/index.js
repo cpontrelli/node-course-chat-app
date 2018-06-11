c7353d4701a313b64df800ee60ff074da299c516
Charlie Pontrelli <cpontrelli@equilar.com>
2018-06-10 18:25:22 -0700

    Added CSS styles

--- public/js/index.js
+++ public/js/index.js
@@ -19,11 +19,35 @@ socket.on('newMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]')
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
+        messageTextbox.val('')
+    });
+});
 
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function() {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.')
+    }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location');
     });
 });
 
@@ -35,21 +59,4 @@ socket.on('newLocationMessage', function(message) {
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
-})
-
-var locationButton = jQuery('#send-location');
-
-locationButton.on('click', function() {
-    if(!navigator.geolocation) {
-        return alert('Geolocation not supported by your browser.')
-    }
-
-    navigator.geolocation.getCurrentPosition(function (position) {
-        socket.emit('createLocationMessage', {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        })
-    }, function () {
-        alert('Unable to fetch location');
-    });
 });
